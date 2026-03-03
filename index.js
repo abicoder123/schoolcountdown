8fcb3dd34f5575be4683f119c2a541697e620030
abicoder123 <ablor32@icstudents.org>
2026-03-03 14:17:22 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 
+const end = new Date("December 25, 2026").getTime();
+
+setInterval(function() {
+  const now = new Date().getTime();
+  const diff = end - now;
+
+  const days = Math.floor(diff / 1000 / 60 / 60 / 24);
+
+  document.getElementById("timer").textContent =
+    days + " days left";
+}, 1000);
