a7940676297613319c87023b2d86262a1db04992
abicoder123 <ablor32@icstudents.org>
2026-03-05 11:53:00 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-
-const end = new Date("March 14, 2026").getTime();
+const end = new Date("March 14, 2026 00:00:00").getTime();
 
 setInterval(function() {
   const now = new Date().getTime();
   const diff = end - now;
 
-  const days = Math.floor(diff / 1000 / 60 / 60 / 24);
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((diff / (1000 * 60)) % 60);
+  const seconds = Math.floor((diff / 1000) % 60);
 
   document.getElementById("timer").textContent =
-    days + " days left";
+    days + "d " +
+    hours + "h " +
+    minutes + "m " +
+    seconds + "s";
 }, 1000);
